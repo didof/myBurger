07739f7ad230a9203e14e99b10b1f2d51135e603
prostagma-code <didonato.fr@gmail.com>
2020-03-11 18:48:01 +0100

    inital commit

---
+++ src/components/Test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './Test.module.css'
+
+const test = () => {
+    return (
+        <div className={styles.dark}>test</div>
+    )
+}
+
+export default test
